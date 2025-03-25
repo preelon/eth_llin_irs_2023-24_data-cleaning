acbf65db48eb9b2b3afbd0684a5c6f694147e844
preelon <enkuelon@gmail.com>
2025-03-25 11:47:22 +0300

    performed geographical cleaning on the 2024 IRS dataset

--- 2024_irs_data_cleaning.R
+++ 2024_irs_data_cleaning.R
@@ -22,6 +22,47 @@ table(irs_2024$id_1082, useNA = "always") #32 entries
 #standardizing the 2023 llin dataset
 irs_2024 <- irs_2024 |>
   janitor::clean_names() |>
+  select(-id_1082) |>
   mutate(region_1082= str_to_title(region_1082),
          zone_1082= str_to_title(zone_1082),
          woreda_1082= str_to_title(woreda_1082))
+
+#joining the IRS_2024 data to the shape file
+irs_2024_sf_joined <- irs_2024 |>
+  left_join(shape_file, by= c("region_1082"= "region",
+                              "zone_1082"= "zone",
+                              "woreda_1082" = "woreda"))
+
+#lets see if there are unmatched districts
+nm <- irs_2024_sf_joined |>
+  filter(is.na(id_1082)) # there are 6 unmatched 
+
+#lets deal with the 6 unmatched observations
+irs_2024 <- irs_2024 |>
+  mutate(zone_1082= case_when(woreda_1082 %in% c("Shinile", 
+                                                 "Gota-Biki",
+                                                 "Erer (Sm)") ~ "Siti",
+                              woreda_1082== "Awra (Af)" ~ "Fanti /Zone 4",
+                              woreda_1082 %in% c("Dulecha","Hanruka")  ~ "Gabi /Zone 3",
+                              TRUE ~ zone_1082))
+                            
+#lets re-join and see if there are still unmatched observations
+irs_2024_sf_joined <- irs_2024 |>
+  left_join(shape_file, by= c("region_1082"= "region",
+                              "zone_1082"= "zone",
+                              "woreda_1082" = "woreda"))
+
+nm <- irs_2024_sf_joined |>
+  filter(is.na(id_1082)) # 0 unmatched 
+
+#lets select the required columns before saving the cleaned data
+irs_2024_sf_joined <- irs_2024_sf_joined |>
+  select(id_1082, year,
+         region= region_1082,
+         zone= zone_1082,
+         woreda= woreda_1082,
+         irs_sprayed_hhs = irs_hs,
+         source= souce)
+
+#saving the cleaned data
+write.csv(irs_2024_sf_joined, "data/processed/2024_irs_cleaned.csv")
